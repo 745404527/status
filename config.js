2673df3369a6f97823985271730d70e1bf500542
745404527 <745404527@qq.com>
2022-10-22 18:16:15 +0800

    初始构建

---
+++ config.js
@@ -0,0 +1,35 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Status',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
+  ApiKeys: [
+    'ur1237524-1751f3b43ea399513c12e037'
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/745404527'
+    },
+    {
+      text: 'blog',
+      url: 'https://blog.sevenwuying.eu.org'
+    }
+  ]
+};
